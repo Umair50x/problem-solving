b226d1f24f66e3afc692ab1424002272a416f7bb
Umair50x <i2u1@outlook.com>
2022-11-13 21:46:04 +0500

    towers of Hanoi problem; unfinished

---
+++ towersOfHanoi.js
@@ -0,0 +1,22 @@
+
+
+/*
+
+Towers of Hanoi problem
+
+    we have 3 towers and several rings
+    The rings need to be in ascending order smallest on top largest on the bottom of a pillar
+    one tower has stack of unordered rings which needs to be shifted from 1st tower to the 3rd tower
+    the 2nd middle tower is a buffer b/w the two; it is going to help us in moving
+
+    //take out all the rings and put them into the middle until we reach the biggest
+    //put the biggest on the 3rd tower
+    //recursively do until all of them are stacked ascending 
+    
+
+*/
+
+
+
+
+
